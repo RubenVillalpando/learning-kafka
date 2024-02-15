5aee353b828c91561f754156faf6999a5d000b5b
RubenVillalpando <mr.villalpando@yahoo.com>
2024-02-15 15:01:02 -0600

    I am able to consume but producer even tho it connects and sends no error, message isn't produced *Insert upside-down smiling face emoji*

--- internal/kafka/kafka.go
+++ internal/kafka/kafka.go
@@ -3,14 +3,15 @@ package kafka
 import (
 	"context"
 	"fmt"
+	"os"
 	"sync"
+	"time"
 
 	"github.com/RubenVillalpando/learning-kafka/internal/model"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 func ProduceMessage() {
-	seeds := []string{"localhost:9092"}
 
 	customer := &model.Customer{
 		ID:    0,
@@ -18,13 +19,15 @@ func ProduceMessage() {
 		Email: "rubenvillalpando1299@gmail.com",
 	}
 	cl, err := kgo.NewClient(
-		kgo.SeedBrokers(seeds...),
+		kgo.WithLogger(kgo.BasicLogger(os.Stderr, kgo.LogLevelInfo, func() string {
+			return time.Now().Format("[2006-01-02 15:04:05.999] ")
+		})),
+		kgo.ConsumeTopics("consumer-info"),
+		kgo.DefaultProduceTopic("consumer-info"),
 	)
-	println("1")
 	if err != nil {
 		panic(err)
 	}
-	println("2")
 	defer cl.Close()
 
 	ctx := context.Background()
@@ -34,23 +37,34 @@ func ProduceMessage() {
 		fmt.Println("error marshaling val: ", err)
 		return
 	}
-	println("3")
 
 	var wg sync.WaitGroup
-	wg.Add(1)
-	record := &kgo.Record{Topic: "customer-info", Value: val, Key: customer.Key()}
-	println("4")
 
-	cl.Produce(ctx, record, func(r *kgo.Record, err error) {
-		println("5")
-		defer wg.Done()
-		if err != nil {
-			fmt.Printf("Record with key {%v} had a produce error: %v\n", r.Key, err)
-		}
-		fmt.Printf("key %v value %v", r.Key, r.Value)
-	})
-	println("7")
+	for i := 0; i < 1; i++ {
+		wg.Add(1)
+		record := &kgo.Record{Topic: "customer-info", Value: val, Key: customer.Key()}
+
+		cl.Produce(ctx, record, func(r *kgo.Record, err error) {
+			defer wg.Done()
+			if err != nil {
+				fmt.Printf("Record with key {%v} had a produce error: %v\n", r.Key, err)
+			}
+			fmt.Printf("key %v value %v\n", r.Key, r.Value)
+		})
+	}
+
 	wg.Wait()
-	println("6")
+	for {
+		fetches := cl.PollFetches(ctx)
+		if errs := fetches.Errors(); len(errs) > 0 {
+			panic(fmt.Sprint(errs))
+		}
+
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+			fmt.Println(string(record.Value), "from an iterator!")
+		}
+	}
 
 }
